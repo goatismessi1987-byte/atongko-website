5581c0d404e3616212dc0498343725b05b358d43
goatismessi1987-byte <goatismessi1987@gmail.com>
2026-01-19 01:40:34 +0600

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,7 +3,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", // আপনার অরিজিনাল কি এখানে থাকবে
+  apiKey: "YOUR_API_KEY", // আপনার অরিজিনাল কি এখানে দিন
   authDomain: "atongko-website.firebaseapp.com",
   projectId: "atongko-website",
   storageBucket: "atongko-website.appspot.com",
@@ -13,4 +13,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const storage = getStorage(app); // এটি ছাড়া বিল্ড ফেইল হবে
+export const storage = getStorage(app);
